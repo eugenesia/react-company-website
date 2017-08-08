eef9b6157c277b075dde43a9c13c649fc3f45340
Eugene Sia <eugene@eugenesia.co.uk>
2017-08-08 10:05:52 +0100

    Use PageTemplate

--- src/pages.js
+++ src/pages.js
@@ -1,39 +1,59 @@
 import { Link } from 'react-router-dom'
+import { MainMenu } from './menus'
 
+// Template for each page. Display children after the main menu.
+const PageTemplate = ({children}) =>
+  <div className="page">
+    <MainMenu />
+    {children}
+  </div>
 
 export const Home = () =>
-  <div className="home">
-    <h1>[Company Website]</h1>
-    <nav>
-      <Link to="about">[About]</Link>
-      <Link to="events">[Events]</Link>
-      <Link to="products">[Products]</Link>
-      <Link to="contact">[Contact Us]</Link>
-    </nav>
-  </div>
-
-export const About = () =>
-  <section className="events">
-    <h1>[About the Company]</h1>
-  </section>
+  <PageTemplate>
+    <div className="home">
+      <h1>[Company Website]</h1>
+      <nav>
+        <Link to="about">[About]</Link>
+        <Link to="events">[Events]</Link>
+        <Link to="products">[Products]</Link>
+        <Link to="contact">[Contact Us]</Link>
+      </nav>
+    </div>
+  </PageTemplate>
 
 export const Events = () =>
-  <section className="events">
-    <h1>[Events Calendar]</h1>
-  </section>
+  <PageTemplate>
+    <section className="events">
+      <h1>[Events Calendar]</h1>
+    </section>
+  </PageTemplate>
 
 export const Products = () =>
-  <section className="products">
-    <h1>[Products Catalog]</h1>
-  </section>
+  <PageTemplate>
+    <section className="products">
+      <h1>[Products Catalog]</h1>
+    </section>
+  </PageTemplate>
 
 export const Contact = () =>
-  <section className="contact">
-    <h1>[Contact Us]</h1>
-  </section>
+  <PageTemplate>
+    <section className="contact">
+      <h1>[Contact Us]</h1>
+    </section>
+  </PageTemplate>
+
+export const About = ({ match }) =>
+  <PageTemplate>
+    <section className="about">
+      <h1>[About the Company]</h1>
+    </section>
+  </PageTemplate>
+
 
 export const Whoops404 = ({ location }) =>
-  <div className="whoops-404">
-    <h1>Resource not found at '{location.pathname}'</h1>
-  </div>
+  <PageTemplate>
+    <div className="whoops-404">
+      <h1>Resource not found at '{location.pathname}'</h1>
+    </div>
+  </PageTemplate>
 
